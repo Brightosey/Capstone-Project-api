9134be7f5dbe8d6ee4b534cd5013865553de00ff
Brightosey <Brightosey@users.noreply.github.com>
2025-03-15 02:41:07 -0400

    Added routes for posts and slides, along with related data files. Added images for posts and slides. Updated index.js and package.json.

---
+++ Routes/slides.js
@@ -0,0 +1,16 @@
+import express from "express";
+import fs from "fs";
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    fs.readFile("./data/slides.json", "utf-8", (error,data) => {
+        if (error) {
+            return res.status(500).send("Error reading data");
+        }
+        res.json(JSON.parse(data));
+    });
+})
+
+
+export default router;
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import slidesRoute from "./Routes/slides.js";
+import postsRoute from "./Routes/posts.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 6060;
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/slides", slidesRoute);
+app.use("/api/newsfeed", postsRoute);
+
+
+app.get("/", (req,res) => {
+    res.send("Jesus is the WAY, the TRUTH, and the LIFE");
+});
+
+app.listen(PORT, (req,res) => {
+    console.log(`The server is running on ${PORT}.`);
+});
+
+    
